4a6fcb75ccdae8e18822b0816bcb02cce5b578f1
Nigonbol123@gmail.com <nigonbol123@gmail.com>
2021-04-04 10:11:21 -0400

    Fixing scoregame value and changing css value

--- script.js
+++ script.js
@@ -5,108 +5,133 @@ let resultTag = document.getElementById("result");
 let rockBtn = document.getElementById("r");
 let paperBtn = document.getElementById("p");
 let scissorBtn = document.getElementById("s");
-let refreshBtn = document.getElementById("btn");
+let pInfo = document.getElementById("game");
 // SELECTION AND SCORE VARIABLES
 let playerSelection;
 let choice = 3; //counter for computer choices
 let userScore = 0;
 let computerScore = 0;
 let finalResult = "";
+let scoreCounter = 0;
 
-
+game();
 // ROCK CHOICE EVENT-LISTENER
+function game() {
+  rockBtn.addEventListener("click", () => {
+    playerSelection = "rock";
+    let computerSelection = computerPlay(choice);
+    if (playerSelection == computerSelection) {
+      userScore += 0;
+      computerScore += 0;
+      finalResult = `It's a draw, nobody wins`;
+      userScoreTag.textContent = userScore;
+      computerScoreTag.textContent = computerScore;
+      resultTag.textContent = finalResult;
+    } else if (playerSelection == "rock" && computerSelection == "paper") {
+      userScore += 0;
+      computerScore += 1;
+      scoreCounter++;
+      finalResult = `computer choice:  ${computerSelection.toUpperCase()} beats your choice: ${playerSelection.toUpperCase()},  you loose`;
+      userScoreTag.textContent = userScore;
+      computerScoreTag.textContent = computerScore;
+      resultTag.textContent = finalResult;
+    } else if (playerSelection == "rock" && computerSelection == "scissor") {
+      userScore += 1;
+      computerScore += 0;
+      scoreCounter++;
+      finalResult = `your choice: ${playerSelection.toUpperCase()} beats computer choice: ${computerSelection.toUpperCase()},  you wins!`;
+      userScoreTag.textContent = userScore;
+      computerScoreTag.textContent = computerScore;
+      resultTag.textContent = finalResult;
+    }
+    if (scoreCounter === 5) {
+      alert("THE GAME IS OVER");
+      setTimeout(function () {
+        location.reload();
+      }, 900);
+      pInfo.textContent = "wait until the page reload";
+    }
+  });
 
-rockBtn.addEventListener("click", () => {
-  playerSelection = "rock";
-  let computerSelection = computerPlay(choice);
-  if (playerSelection == computerSelection) {
-    userScore += 0;
-    computerScore += 0;
-    finalResult = `It's a draw, nobody wins`;
-    userScoreTag.textContent = userScore;
-    computerScoreTag.textContent = computerScore;
-    resultTag.textContent = finalResult;
-  } else if (playerSelection == "rock" && computerSelection == "paper") {
-    userScore += 0;
-    computerScore += 1;
-    finalResult = `computer choice:  ${computerSelection.toUpperCase()} beats your choice: ${playerSelection.toUpperCase()},  you loose`;
-    userScoreTag.textContent = userScore;
-    computerScoreTag.textContent = computerScore;
-    resultTag.textContent = finalResult;
-  } else if (playerSelection == "rock" && computerSelection == "scissor") {
-    userScore += 1;
-    computerScore += 0;
-    finalResult = `your choice: ${playerSelection.toUpperCase()} beats computer choice: ${computerSelection.toUpperCase()},  you wins!`;
-    userScoreTag.textContent = userScore;
-    computerScoreTag.textContent = computerScore;
-    resultTag.textContent = finalResult;
-  }
-});
+  // PAPER CHOICE EVENT-LISTENER
 
-// PAPER CHOICE EVENT-LISTENER
+  paperBtn.addEventListener("click", () => {
+    playerSelection = "paper";
+    let computerSelection = computerPlay(choice);
+    if (playerSelection == computerSelection) {
+      userScore += 0;
+      computerScore += 0;
+      finalResult = `It's a draw, nobody wins`;
+      userScoreTag.textContent = userScore;
+      computerScoreTag.textContent = computerScore;
+      resultTag.textContent = finalResult;
+    } else if (playerSelection == "paper" && computerSelection == "rock") {
+      userScore += 1;
+      computerScore += 0;
+      scoreCounter++;
+      finalResult = `your choice: ${playerSelection.toUpperCase()} beats computer choice: ${computerSelection.toUpperCase()}, you wins`;
+      userScoreTag.textContent = userScore;
+      computerScoreTag.textContent = computerScore;
+      resultTag.textContent = finalResult;
+    } else if (playerSelection == "paper" && computerSelection == "scissor") {
+      userScore += 0;
+      computerScore += 1;
+      scoreCounter++;
+      finalResult = `computer choice: ${computerSelection.toUpperCase()} beats your choice: ${playerSelection.toUpperCase()}, you loose`;
+      userScoreTag.textContent = userScore;
+      computerScoreTag.textContent = computerScore;
+      resultTag.textContent = finalResult;
+    }
+    if (scoreCounter === 5) {
+      alert("THE GAME IS OVER");
+      setTimeout(function () {
+        location.reload();
+      }, 900);
+      pInfo.textContent = "wait until the page reload";
+    }
+  });
 
-paperBtn.addEventListener("click", () => {
-  playerSelection = "paper";
-  let computerSelection = computerPlay(choice);
-  if (playerSelection == computerSelection) {
-    userScore += 0;
-    computerScore += 0;
-    finalResult = `It's a draw, nobody wins`;
-    userScoreTag.textContent = userScore;
-    computerScoreTag.textContent = computerScore;
-    resultTag.textContent = finalResult;
-  } else if (playerSelection == "paper" && computerSelection == "rock") {
-    userScore += 1;
-    computerScore += 0;
-    finalResult = `your choice: ${playerSelection.toUpperCase()} beats computer choice: ${computerSelection.toUpperCase()}, you wins`;
-    userScoreTag.textContent = userScore;
-    computerScoreTag.textContent = computerScore;
-    resultTag.textContent = finalResult;
-  } else if (playerSelection == "paper" && computerSelection == "scissor") {
-    userScore += 0;
-    computerScore += 1;
-    finalResult = `computer choice: ${computerSelection.toUpperCase()} beats your choice: ${playerSelection.toUpperCase()}, you loose`;
-    userScoreTag.textContent = userScore;
-    computerScoreTag.textContent = computerScore;
-    resultTag.textContent = finalResult;
-  }
-});
+  // SCISSOR EVENT-LISTENER
+
+  scissorBtn.addEventListener("click", () => {
+    playerSelection = "scissor";
+    let computerSelection = computerPlay(choice);
+    if (playerSelection == computerSelection) {
+      userScore += 0;
+      computerScore += 0;
+      finalResult = `It's a draw, nobody wins`;
+      userScoreTag.textContent = userScore;
+      computerScoreTag.textContent = computerScore;
+      resultTag.textContent = finalResult;
+    } else if (playerSelection == "scissor" && computerSelection == "paper") {
+      userScore += 1;
+      computerScore += 0;
+      scoreCounter++;
+      finalResult = `your choice: ${playerSelection.toUpperCase()} beats computer choice: ${computerSelection.toUpperCase()}, you wins`;
+      userScoreTag.textContent = userScore;
+      computerScoreTag.textContent = computerScore;
+      resultTag.textContent = finalResult;
+    } else if (playerSelection == "scissor" && computerSelection == "rock") {
+      userScore += 0;
+      computerScore += 1;
+      scoreCounter++;
+      finalResult = `computer choice: ${computerSelection.toUpperCase()} beats your choice: ${playerSelection.toUpperCase()}, you loose`;
+      userScoreTag.textContent = userScore;
+      computerScoreTag.textContent = computerScore;
+      resultTag.textContent = finalResult;
+    }
+    if (scoreCounter === 5) {
+      alert("THE GAME IS OVER");
+      setTimeout(function () {
+        location.reload();
+      }, 900);
+      pInfo.textContent = "wait until the page reload";
+    }
+  });
+}
 
 // Reload Buttom
 
-refreshBtn.addEventListener("click",()=>{
-  location.reload();
-})
-
-
-// SCISSOR EVENT-LISTENER
-
-scissorBtn.addEventListener("click", () => {
-  playerSelection = "scissor";
-  let computerSelection = computerPlay(choice);
-  if (playerSelection == computerSelection) {
-    userScore += 0;
-    computerScore += 0;
-    finalResult = `It's a draw, nobody wins`;
-    userScoreTag.textContent = userScore;
-    computerScoreTag.textContent = computerScore;
-    resultTag.textContent = finalResult;
-  } else if (playerSelection == "scissor" && computerSelection == "paper") {
-    userScore += 1;
-    computerScore += 0;
-    finalResult = `your choice: ${playerSelection.toUpperCase()} beats computer choice: ${computerSelection.toUpperCase()}, you wins`;
-    userScoreTag.textContent = userScore;
-    computerScoreTag.textContent = computerScore;
-    resultTag.textContent = finalResult;
-  } else if (playerSelection == "scissor" && computerSelection == "rock") {
-    userScore += 0;
-    computerScore += 1;
-    finalResult = `computer choice: ${computerSelection.toUpperCase()} beats your choice: ${playerSelection.toUpperCase()}, you loose`;
-    userScoreTag.textContent = userScore;
-    computerScoreTag.textContent = computerScore;
-    resultTag.textContent = finalResult;
-  }
-});
 
 
 // COMPUTER RANDOM FUNCTION CHOICE
